4e2ddf780c9c8db620a8cf404b8b222a9ad7863f
Rafael <rafinha_fern@hotmail.com>
2022-07-18 11:51:22 -0300

    feat: user can get credentials

--- src/routes/credentialsRouter.ts
+++ src/routes/credentialsRouter.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
+import getCredentialsCtr from "../controllers/credentials/getCredentialsCtr.js";
 import newCredentialCtr from "../controllers/credentials/newCredentialCtr.js";
 import newCredentialSchemaMd from "../middlewares/schemas/credentials/newCredentialSchemaMd.js";
 import tokenBearerMd from "../middlewares/tokenBearerMd.js";
 
 const credentialsRouter = Router();
 
-credentialsRouter.post('/credentials',tokenBearerMd,newCredentialSchemaMd,newCredentialCtr)
+credentialsRouter.post('/credentials',tokenBearerMd,newCredentialSchemaMd,newCredentialCtr);
+credentialsRouter.get('/credentials',tokenBearerMd,getCredentialsCtr);
 
 export default credentialsRouter;
